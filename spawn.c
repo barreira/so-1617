7c3f9eddf7545d85c4e60d99fce41f655c07d012
José Moreira <diskoballs@gmail.com>
2017-05-13 20:25:05 +0100

    adicionado para executar o  no comando, ver limitações no TODO

--- spawn.c
+++ spawn.c
@@ -13,59 +13,59 @@ e acrescentando uma nova coluna com o respetivo exit status.
 spawn mailx -s $3 x@y.com
 substituir $n pelo valor da coluna
 
-grep -Eo [\$1-9]{2} cmds.txt
+./a.out mailx -s \$3 x@y.com 
+
+
 */
 
 int main(int argc, char const *argv[]){
 
 	int total = argc-1;
-	int coluna =0; //guardar os $n
+	int coluna =0, achei=0; //guardar o $n
 	int i,n,cut,s, pid,status;
 	char *cmd[total]; //guarda o comando a executar
-	//char *tmp;
+	char *tmp;
 	char buffer[PIPE_BUF];
 	char print[PIPE_BUF];
 	char final[PIPE_BUF];
-	char col[10];
-	char field[10];
+	char field[PIPE_BUF];
 
+	//verificação de erros do numero de argumentos (ou assume-se que o input é sempre correcto?)
+	if(argc < 2) { write(2,"Sem argumentos!!",16); return 2; }
 
 	//passar argumentos para array ; remover ./a.out
-	//--argc;
-	//++argv;
-	for(i=0;i<total;i++) {
-		    if(i+1 == total) cmd[total+1] = NULL;
+	//--argc; ++argv; Dava se não fosse preciso alterar os argvs para o n da coluna a executar
+	for(i=0;i<argc;i++) {
+		    if(i+1 == argc) cmd[argc] = NULL;
 		    else cmd[i] = argv[i+1];
 		}
-	//verify for $n
-	//char *tmp = cmd;
+	//verificar se $n aparece, se sim, remover $ e guardar a sua respectiva coluna 
+	for(i=0;i<total;i++) {
+		tmp = cmd[i]; 
+		if (*tmp == '$'){ // se começa por $
+			tmp++;    // avanço um caracter e fico a apontar para o primeiro dígito
+			coluna = atoi(tmp); //número da coluna armazenado em coluna.
+			achei = 1;
+			break; //sair do ciclo ao achar
+		}
+		coluna++;
+	}
 
-	//for(i=0;i<total;i++) {
-	// 	if(strcmp(cmd[i][0],"$") ==0) { cmd[i][0] = cmd[i][1] ; cmd[i][1] = '\0'; coluna = atoi(cmd[i]);}
-	//}
-
-	//for(i=0;i<total;i++) { write(1,argv[i],strlen(argv[i])); }
-	//guardar valor coluna a usar no argumento
-
+	//processar input
 	while(n = read(0,buffer,PIPE_BUF)) {      
-    	buffer[n-1] = '\0'; //tirar /n
-    	//Achar a coluna
-    	/*
+    	//Achar a(s) coluna(s)
     	char *ptr = buffer;
     	cut = 0;
-    	while ( sscanf(ptr, "%10[^:]%n", field, &s) == 1) {
+    	while ( sscanf(ptr, "%[^:]%n", field, &s) == 1) {
         	cut++;
         	if(cut == coluna) sprintf(print,"%s",field); //achou a coluna, guardar valor no print
         	ptr += s; // avançar os characteres lidos
-        	//if ( *ptr != ':' )  {  break; // falhou }
          	++ptr; // salta o : 
       	}
-      	*/
-		//trabalhar linha recebida
-		//argv[coluna-1] = print;
-    	//cmd[coluna-1] = print; //valor da coluna no comando a executar
-    	//filho executar comando
-      	pid = fork();
+		//mudar comando a executar com valor da coluna já convertido, caso haja algum $n
+    	if(achei) cmd[coluna] = print; 
+    	//faz fork e o filho executa o comando com os valores alterados
+      	pid = fork(); //guardar pid filho
       	if(pid==0) { 
       		int devNull = open("/dev/null", O_WRONLY); 
       		dup2(devNull,1); //mandar output para /dev/null
@@ -74,10 +74,8 @@ int main(int argc, char const *argv[]){
       	}
 		//pai faz waitpid e guarda exit status
      	waitpid(pid,&status,0);
-      	if(WIFEXITED(status)) { sprintf(final,"%s:%i\n",buffer,WEXITSTATUS(status)); }
-		//print line
+     	buffer[n-1] = '\0'; //tirar /n
+      	if(WIFEXITED(status)) { sprintf(final,"%s:%i\n",buffer,WEXITSTATUS(status)); } //adicionar o exit status
 		write(1,final,strlen(final));
-		//print command
-		//for(i=0;i<total;i++) { write(1,cmd[i],strlen(cmd[i])); }
 	}
 }
