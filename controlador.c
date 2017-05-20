40e00054e49d617aafe925152fca66fb20c7f06c
José Moreira <diskoballs@gmail.com>
2017-05-20 14:13:07 +0100

    node, fannout e connect feitos mas com erros

--- controlador.c
+++ controlador.c
@@ -50,101 +50,6 @@ int exec_component(char** cmd)
 // options[5] = "10"
 
 /*
-int node(char** options) // e.g node 1 window 2 avg 10
-{   
-    int i, nb1, nb2, nb3;
-    int pid, first, second, third;
-    int wr[2], rd[2];
-    char buff_rd[MAX_SIZE], buff_wr[MAX_SIZE], buff_io[MAX_SIZE];
-    char* component = "", pipe_name = "pipes/io/";
-
-    for (i = 2; options[i] != NULL; i++) {
-        strcat(component, options[i]);
-        strcat(component, " ");
-    }
-
-    pid = fork();
-
-    if (pid == -1) {
-        perror("fork");
-        return 1;
-    }
-
-    if (pid == 0) {
-        pipe(wr);
-        pipe(rd);
-
-        first = fork();
-
-        if (first == 0) { // leitor
-            close(wr[0]);
-            close(rd[0]);
-            close(rd[1]);
-
-            while ((nb1 = read(0, buff_rd, MAX_SIZE)) > 0) { // tem de ser readln
-                write(wr[1], buff_rd, nb1);
-            }
-
-            _exit(0);
-        }
-
-        second = fork();
-
-        if (second == 0) { // filtro
-            dup2(wr[0], 0);
-            dup2(rd[1], 1);
-
-            close(wr[1]);
-            close(rd[0]);
-
-            exec_component(&options[2]);
-
-            _exit(0);
-        }
-
-        third = fork();
-
-        if (third == 0) { // escritor
-            close(rd[1]);
-            close(wr[0]);
-            close(wr[1]);
-
-            while ((nb2 = read(rd[0], buff_wr, MAX_SIZE)) > 0) { // tem de ser readln
-                write(n->out, buff_wr, nb2);
-            }
-
-            _exit(0);
-        }
-
-        close(wr[0]);
-        close(wr[1]);
-        close(rd[0]);
-        close(rd[1]);
-
-        // Nó vai estar sempre a ler do fifo das mudanças de IO
-        strcat(pipe_name, itoa(getpid()));
-        fifo_io = open(pipe_name, O_RDONLY);
-
-        while ((nb3 = read(fifo_io, buff_io, MAX_SIZE)) > 0) {
-            change_io();
-        }
-    }
-    else {
-        // Cria-se uma nova struct Node para guardar as informações do nó criado
-        Node n = create_node(atoi(options[0]), pid, component);
-        // Adiciona-se o novo nó à lista ligada dos nós (do controlador)
-        network = g_slist_append(network, n);
-
-        // Cria-se o fifo que servirá para o controlador comunicar aos nós as mudanças de IO (aquando do connect)
-        strcat(pipe_name, itoa(pid));
-        mkfifo(pipe_name, 0666);
-    }
-
-    return 0;
-}
-*/
-
-
 int connect(char** options)
 {
     // connect 2 3 5
@@ -181,7 +86,7 @@ int disconnect(char** options)
 
     //close_connections(network, connections);
 }
-
+*/
 int inject(char** options) {  }
 /*
 int interpretador(char* cmdline)
@@ -227,21 +132,13 @@ int main(int argc, char* argv[])
     int fd1;
     char* buffer;
     
-    //network = NULL;
-
     if (argc == 2) {
         // ler ficheiro de configuração (se existir) e executar linha a linha
+
+        //avisar que carregou com sucesso ?
     }
     /*
-    if (mkfifo("fifo", 0666) == -1) {
-        perror("mkfifo");
-    }
-
-    if ((fd1 = open("fifo", O_RDONLY)) == -1) {
-        perror("open");
-        return 1;
-    }
-
+    //loop leitura de comandos
     while(1) {
         while((n = read(0, buffer, MAX_SIZE)) > 0 ) {
             interpretador(buffer);
@@ -305,43 +202,47 @@ void connect(char nodo, char out[]) {
 	fnum++; //aumentar fnum
 	a = atoi(nodo);
 	for(i=0;i<MAX_SIZE;i++) { //verificar nos fanouts se algum está a ler do nodo ; se sim: matar fanout e criar fanout com novos valores
-		if(fin[i] == a) { stopfan(a); break; } 
+		if(fin[i] == a) { signal(fpid[i],SIGUSR1); break; } 
 	}
 	//fork, correr fanout, guardar pid fannout para mandar signal
-	fork();
-	actualizar fin[fnum] = a;
-	fanout(a, out[],fnum); //criar fannout n para usar no fstop
+	fpid[fnum] = fork();
+	if(fpdi[fnum] == 0) {
+	fin[fnum] = a; //fannout num fnum recebe input nodo a
+	fanout(nodo, out[],fnum); //criar fannout n para usar no fstop
+	}
 }
-
-
-
-
-
 // ############# CONNECT END ######################################
 
 // #### FANOUT ###############################
-/*
-void fanout(char in, char out[], int n) // in = fifo in , out = fifo(s) out, n = fnum number
+void fanout(char in, char outputs[], int n) // in = fifo in , out = fifo(s) out, n = fnum number
 {
-    signal(SIGUSR1, stopfan);
+    signal(SIGUSR1, stopfan); //pára o fanout na próxima iteração, inicio da main ?
     char in[15],out[15];
-    int fdi,fdo;
-    sprintf(in,"./tmp/%sin",num); // Nin
-    sprintf(out,"./tmp/%sout",num); //Nout
-    int bytes, i;
+	int bytes, i,osize=1;
     char buffer[MAX_SIZE];
-    reader = open()
+    //input
+    sprintf(in,"./tmp/%sin",num); // Nin
+    inputs = open(in,O_RDONLY); //abrir fifo entrada leitura
+    //descobrir número outputs
+    for(i=0;i<osize;i++) { if(outputs[i] != '\0') osize++ ; }
+    //abrir fifos outputs para escrita
+    int prints[osize];
+    for(i=0;i<osize;i++) { 
+    	sprintf(out,"./tmp/%sout",outputs[i]); //Nout
+    	prints[i] = open(out,O_WRONLY); //verificar se abriu ?
+    }
+    //loop leitura e escrita em todos os outputs
+    while ((bytes = read(inputs, buffer, PIPE_BUF)) > 0 && (fstop[n] == 0)) {
+    	//readline
 
-    while ((bytes = read(in, buffer, MAX_SIZE)) > 0 && running) {
-        // executa comando
-        for (i = 0; i < n; i++) {
-            write(out[i], buffer, bytes);
+        // faz os writes em todos os outputs (prints[])
+        for (i = 0; i < osize; i++) {
+            write(prints[i], buffer, bytes);
         }
     }
+    //fechar fifos escrita/leitura ao receber signal para fstop ? ou não é preciso ?
+    //if (fstop[n] == 1) for ... close(...)
 }
-
-*/
-
 //##################FANOUT END ##########################
 
 
