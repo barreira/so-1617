4eaf6ed5216a97dc6b00a0adf80df4794a740f3c
José Moreira <diskoballs@gmail.com>
2017-05-21 18:18:39 +0100

    modificado para não pararem com EOF

--- const.c
+++ const.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <limits.h>
 
+
 /* Este programa reproduz as linhas acrescentando uma nova coluna sempre com o mesmo valor: 
 utilização ./a.out const
 input: a:b:c
@@ -15,14 +16,18 @@ output: a:b:c:const
 
 int main(int argc, char const *argv[]){
 
+	
+
 	char buffer[PIPE_BUF];
 	char print[PIPE_BUF];
 	int n;
 
-	while((n = read(0,buffer,PIPE_BUF))) {		
+	while((n = read(0,buffer,PIPE_BUF)) >= 0) {	
+		if(n!=0) {
 		buffer[n-1] = '\0'; //tirar /n
 		sprintf(print,"%s:%s\n",buffer,argv[1]); //acrescentar resto :const
 		write(1,print,strlen(print)); //write stdout
+		}
 	}
 
   return 0; //nunca aqui vai chegar, mas é menos um warning ao compilar
