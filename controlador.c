b85b51a8a69af9bc921da04506d5740efedc3022
João Pires Barreira <jpiresbarreira@gmail.com>
2017-06-01 20:02:37 +0100

    Removidos comentários de debug

--- controlador.c
+++ controlador.c
@@ -544,35 +544,15 @@ int remove_node(char** options) {
        que queremos remover como OUT */
 
     for (i = 0; i < MAX_SIZE; i++) {
-        if (connections[i] != NULL) { 
+        if (connections[i] != NULL) {
             numouts = connections[i]->numouts;
-            //printf("numouts = %d",numouts);
 
             for(j = 0; j < numouts ; j++) {
-                //printf("j= %d i= %d\n",j,i);
 
                 /* Caso uma conexão tenha o nó como OUT, faz-se o disconnect
                    entre esse nó e o nó da entrada dessa conexão */
 
-                // printf("j= %d ; connections[i]->outs[0] = %d\n",j, connections[i]->outs[0]);
-                // printf("j= %d ; connections[i]->outs[1] = %d\n",j, connections[i]->outs[1]);
-                // printf("j= %d ; connections[i]->outs[2] = %d\n",j, connections[i]->outs[2]);
-                // write(1,"seg fault\n",9);
-                // printf("j= %d vai entrar no if",j);
                 if (connections[i]->outs[j] == a) {
-                    // printf("j= %d\n",j);
-
-                    /* Criar array de options do disconnect e sua executá-lo */
-                    
-                    // printf("disconnect:\n");
-                    // ############################################################################ é aqui ao criar o options!!
-
-                    // sprintf(args, "disconnect %d %d", i, j); //isto aqui resolver
-
-                    // args[2] = options[1];
-                    // printf("disconnect run:\n");
-
-                    // disconnect i j
             	    args[0] = strdup("disconnect");
                     sprintf(tmp, "%d", i);
                     args[1] = strdup(tmp);
@@ -580,8 +560,8 @@ int remove_node(char** options) {
 
                     disconnect(args);
                     break;
-		}
-             } 
+		        }
+            } 
     	}
     }
 
