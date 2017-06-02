c8a38b94a4264009c5916b47f3484448ed22e6ad
João Pires Barreira <jpiresbarreira@gmail.com>
2017-06-02 23:56:46 +0100

    Correções de sintaxe nos filtros

--- const.c
+++ const.c
@@ -9,31 +9,27 @@
 
 #include "readln.h"
 
-/* Este programa reproduz as linhas acrescentando uma nova coluna sempre com o mesmo valor: 
-utilização ./a.out const
-input: a:b:c
-output: a:b:c:const
-*/
 
-int main(int argc, char const *argv[]){
-
-	
-
+/*
+ * @brief Este programa reproduz as linhas acrescentando uma nova coluna sempre com o
+ * mesmo valor.
+ *
+ * Utilização: ./a.out const 
+ *
+ * input:  a:b:c
+ * output: a:b:c:const
+ */ 
+int main(int argc, char const *argv[]) {
 	char buffer[PIPE_BUF];
 	char print[PIPE_BUF];
-
 	int n;
 
-	while((n = readln(0,buffer,PIPE_BUF)) >= 0) {	
-		if(n!=0) {
-
-		sprintf(print,"%s:%s\n",buffer,argv[1]); //acrescentar resto :const
-		write(1,print,strlen(print)); //write stdout
-				
+	while ((n = readln(0, buffer, PIPE_BUF)) >= 0) {	
+		if (n != 0) {
+			sprintf(print, "%s:%s\n", buffer, argv[1]); // acrescentar resto :const
+			write(1, print, strlen(print));
 	   }
-
 	}
 
-  return 0; //nunca aqui vai chegar, mas é menos um warning ao compilar
-
+	return 0;
 }
--- spawn.c
+++ spawn.c
@@ -20,12 +20,11 @@ substituir $n pelo valor da coluna
 
 
 */
-
 int main(int argc, char const *argv[]){
 
 	int total = argc-1;
 	int coluna =0, achei=0; //guardar o $n
-	int i,n,cut,s, pid,status;
+	int i, n, cut, s, pid,status;
 	char *cmd[total]; //guarda o comando a executar
 	char *tmp;
 	char buffer[PIPE_BUF];
@@ -38,12 +37,12 @@ int main(int argc, char const *argv[]){
 
 	//passar argumentos para array ; remover ./a.out
 	//--argc; ++argv;
-	for(i=0;i<argc;i++) {
-		    if(i+1 == argc) cmd[argc] = NULL;
+	for (i = 0; i < argc; i++) {
+		    if (i+1 == argc) cmd[argc] = NULL;
 		    else cmd[i] = argv[i+1];
-		}
+	}
 	//verificar se $n aparece, se sim, remover $ e guardar a sua respectiva coluna 
-	for(i=0;i<total;i++) {
+	for(i = 0; i < total; i++) {
 		tmp = cmd[i]; 
 		if (*tmp == '$'){ // se começa por $
 			tmp++;    // avanço um caracter e fico a apontar para o primeiro dígito
@@ -55,36 +54,35 @@ int main(int argc, char const *argv[]){
 	}
 
 	//processar input
-   while((n = readln(0,buffer,PIPE_BUF)) >= 0) {  
-      if(n!=0) {   
-    	//Achar a(s) coluna(s)
-    	char *ptr = buffer;
-    	cut = 0;
-    	while ( sscanf(ptr, "%[^:]%n", field, &s) == 1) {
-        	cut++;
-        	if(cut == coluna) sprintf(print,"%s",field); //achou a coluna, guardar valor no print
-        	ptr += s; // avançar os characteres lidos
-         	++ptr; // salta o : 
-      	}
-		//mudar comando a executar com valor da coluna já convertido, caso haja algum $n
-    	if(achei) cmd[coluna] = print; 
-    	//faz fork e o filho executa o comando com os valores alterados
-      	pid = fork(); //guardar pid filho
-      	if(pid==0) { 
-      		int devNull = open("/dev/null", O_WRONLY); 
-      		dup2(devNull,1); //mandar output para /dev/null
-      		dup2(devNull,2); //stderr putput para /dev/null
-      		execvp(cmd[0],cmd); 
-      	}
-		//pai faz waitpid e guarda exit status
-     	waitpid(pid,&status,0);
-     	//buffer[n-1] = '\0'; //tirar /n
-      	if(WIFEXITED(status)) { sprintf(final,"%s:%i\n",buffer,WEXITSTATUS(status)); } //adicionar o exit status
-		write(1,final,strlen(final));
-	}
-	//else { pause(); }
+    while ((n = readln(0, buffer, PIPE_BUF)) >= 0) {  
+        if (n != 0) {   
+    	    //Achar a(s) coluna(s)
+    	    char *ptr = buffer;
+    	    cut = 0;
+    	    while (sscanf(ptr, "%[^:]%n", field, &s) == 1) {
+        	    cut++;
+        	    if(cut == coluna) sprintf(print,"%s",field); // achou a coluna, guardar valor no print
+        	    ptr += s; // avançar os characteres lidos
+         	    ++ptr; // salta o : 
+      	    }
+
+		    // mudar comando a executar com valor da coluna já convertido, caso haja algum $n
+    	    if (achei) cmd[coluna] = print; 
+    	    // faz fork e o filho executa o comando com os valores alterados
+      	    pid = fork(); //guardar pid filho
+      	    if (pid == 0) { 
+      		    int devNull = open("/dev/null", O_WRONLY); 
+      		    dup2(devNull, 1); //mandar output para /dev/null
+      		    dup2(devNull, 2); //stderr putput para /dev/null
+      		    execvp(cmd[0], cmd); 
+      	    }
+		    // pai faz waitpid e guarda exit status
+     	    waitpid(pid, &status, 0);
+     	    // buffer[n-1] = '\0'; //tirar /n
+      	    if (WIFEXITED(status)) { sprintf(final, "%s:%i\n", buffer, WEXITSTATUS(status)); } // adicionar o exit status
+		    write(1, final, strlen(final));
+	    }
  	}
 
-  return 0; //nunca aqui vai chegar, mas é menos um warning ao compilar
-
+    return 0;
 }
