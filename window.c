2182e6a929648dd32456f7548c036e402bb8225f
João Pires Barreira <jpiresbarreira@gmail.com>
2017-06-03 04:06:51 +0100

    Adicionados mais comentários ao spawn

--- window.c
+++ window.c
@@ -9,41 +9,39 @@
 
 #include "readln.h"
 
-/*window <coluna> <operacao> <linhas>
-Este programa reproduz todas as linhas acrescentando-lhe uma nova coluna com o resultado de uma
-operacão  é calculada sobre os valores da coluna indicada nas linhas anteriores.
-avg, max, min, sum
-
-./a.out 1 sum 3
-
-input: 10:a:b
-output: 10:a:b:10
-
-input: 10
-output: 10:20
-
-input: 5:c:d
-output: 5:c:d:25
-*/
-
-int main(int argc, char const *argv[]){
-
-    int linhas = atoi(argv[3]);
-	int coluna = atoi(argv[1]);
+/*
+ * @brief Este programa reproduz todas as linhas acrescentando-lhe uma nova
+ * coluna com o resultado de uma operacão (avg, max, min, sum) é calculada sobre
+ * os valores da coluna indicada nas linhas anteriores.
+ * 
+ *        e.g. window <coluna> <operacao> <linhas>
+ *
+ * Utilização: ./a.out 1 sum 3
+ *
+ * input: 10:a:b
+ * output: 10:a:b:10
+ *
+ * input: 10
+ * output: 10:20
+ *
+ * input: 5:c:d
+ * output: 5:c:d:25
+ */
+int main(int argc, char const *argv[]) {
+	int cut, n, i, res, first = 0, s, linhas, colunas;
+	char buffer[PIPE_BUF], print[PIPE_BUF], final[PIPE_BUF], field[100];
 	int stored[linhas]; //armazena valores
-	int cut,n,i,res,first=0,s;
-	char buffer[PIPE_BUF];
-	char print[PIPE_BUF];
-	char final[PIPE_BUF];
-	char field[100];
-	
+
+	linhas = atoi(argv[3]);
+	coluna = atoi(argv[1]);
+
 	// AVG
     int do_avg() {
-		if (first == 0) { first++; return 0; } //quando começa dá sempre 0
-		if (first == 1) { first++; return stored[0]; } //quando só tem uma coluna dá o valor dessa coluna
-		else if (first < linhas) { //a partir dai enquanto que não houver buffer, utiliza as x linhas existentes
+		if (first == 0) { first++; return 0; } // quando começa dá sempre 0
+		if (first == 1) { first++; return stored[0]; } // quando só tem uma coluna dá o valor dessa coluna
+		else if (first < linhas) { // a partir dai enquanto que não houver buffer, utiliza as x linhas existentes
 			     int avg = 0;
-				 for(i = 0; i < first; i++) {
+				 for (i = 0; i < first; i++) {
 				 	avg = avg + stored[i];
 				 }
 				 avg = avg / first;
@@ -52,7 +50,7 @@ int main(int argc, char const *argv[]){
 		}
 
 		int avg = 0;
-		for(i = 0; i < linhas; i++) {
+		for (i = 0; i < linhas; i++) {
 			avg = avg + stored[i];
 		}
 		avg = avg / linhas;
@@ -61,10 +59,10 @@ int main(int argc, char const *argv[]){
 
 	//MAX
     int do_max() {
-		if (first == 0) { first++; for(i = 0; i < linhas; i++) { stored[i] = res; } } // incialização para máximo
+		if (first == 0) { first++; for (i = 0; i < linhas; i++) { stored[i] = res; } } // incialização para máximo
 		int max;
 		max = stored[0];
-		for(i = 1; i < linhas; i++) {
+		for (i = 1; i < linhas; i++) {
 			if (max < stored[i]) max = stored[i];
 		}
 		return max;
@@ -72,10 +70,10 @@ int main(int argc, char const *argv[]){
 
 	//MIN
     int do_min() {
-		if (first == 0) { first++; for(i = 0; i < linhas; i++) { stored[i] = res; } } // incialização para min
+		if (first == 0) { first++; for (i = 0; i < linhas; i++) { stored[i] = res; } } // incialização para min
 		int min;
 		min = stored[0];
-		for(i = 1; i < linhas; i++) {
+		for (i = 1; i < linhas; i++) {
 			if (min > stored[i]) min = stored[i];
 		}
 		return min;
@@ -83,9 +81,9 @@ int main(int argc, char const *argv[]){
 
 	//SUM
 	int do_sum() {
-		if (first == 0) { for(i = 1; i < linhas; i++) { stored[i] = 0; } first++; } // inicializar todos os valores a 0
+		if (first == 0) { for (i = 1; i < linhas; i++) { stored[i] = 0; } first++; } // inicializar todos os valores a 0
 		int sum = 0;
-		for(i = 0; i < linhas; i++) {
+		for (i = 0; i < linhas; i++) {
 			sum = sum + stored[i];
 		}
 		return sum;
@@ -93,15 +91,15 @@ int main(int argc, char const *argv[]){
 
 	// faz operação
 	void do_op() {
-		if (strcmp(argv[2],"avg") == 0) res = do_avg();
-		if (strcmp(argv[2],"max") == 0) res = do_max();
-		if (strcmp(argv[2],"min") == 0) res = do_min();
-		if (strcmp(argv[2],"sum") == 0) res = do_sum();
+		if (strcmp(argv[2], "avg") == 0) res = do_avg();
+		if (strcmp(argv[2], "max") == 0) res = do_max();
+		if (strcmp(argv[2], "min") == 0) res = do_min();
+		if (strcmp(argv[2], "sum") == 0) res = do_sum();
 	}
 
 	// adiciona um novo valor ao array e tira o mais antigo
 	void novo_valor(int a){
-		for(i = linhas; i >= 1; i--) {
+		for (i = linhas; i >= 1; i--) {
 			stored[i] = stored[i-1];
 		}
 		stored[0] = a;
@@ -111,21 +109,22 @@ int main(int argc, char const *argv[]){
         if (n != 0) {  
          
             //Achar a coluna
+
             char *ptr = buffer;
             cut = 0;
+
             while (sscanf(ptr, "%100[^:]%n", field, &s) == 1) {
                 cut++;
-                if (cut == coluna) sprintf(print, "%s\n", field); //achou a coluna, guardar valor no print
-                ptr += s; /* avançar os characteres lidos */
-                ++ptr; /* salta o : */
+                if (cut == coluna) sprintf(print, "%s\n", field); // achou a coluna, guardar valor no print
+                ptr += s; // avançar os characteres lidos
+                ++ptr; // salta o :
             }
         
-		    //fazer as operações
-            res = atoi(print); //guardar valor para inteiro
-            novo_valor(res); //adiciona novo valor
-            do_op(); //faz as contas e actualiza res(ultado)
-            //buffer[n-1] = '\0'; //tirar /n
-            sprintf(final, "%s:%i\n", buffer, res); //acrescentar resultado fim da linha
+		    // fazer as operações
+            res = atoi(print); // guardar valor para inteiro
+            novo_valor(res); // adiciona novo valor
+            do_op(); // faz as contas e actualiza res(ultado)
+            sprintf(final, "%s:%i\n", buffer, res); // acrescentar resultado fim da linha
             write(1, final, strlen(final));
         }
     }
